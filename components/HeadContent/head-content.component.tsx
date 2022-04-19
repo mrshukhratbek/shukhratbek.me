39f872c26289c9b9e3bb2bf7bf3ffd014a8ba1ff
mrshuhrat <shuhratadm1n@gmail.com>
2022-04-19 14:48:20 +0500

    feat: change some default codes

---
+++ components/HeadContent/head-content.component.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Shukhratbek from '@imgs/shukhratbek.jpg';
+
+export const HeadContent = () => {
+  return (
+    <Head>
+      <title>Shuhratbek`s Blog</title>
+      <meta name="author" content="Shukhratbek Kobulov" />
+      <meta property="og:site_name" content="Shukhratbek`s Blog" />
+
+      <meta
+        name="description"
+        content="I am Shukhratbek Kobulov, a 20-year-old Software Engineer from Tashkent, Uzbekistan."
+      />
+      <meta name="og:image" content={`${Shukhratbek.src}`}></meta>
+
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
