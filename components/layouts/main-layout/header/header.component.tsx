3cb7e492de0177cda06edaadeffb9864dd1a3204
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-06-25 02:50:31 +0500

    feat: create html element

--- components/layouts/main-layout/header/header.component.tsx
+++ components/layouts/main-layout/header/header.component.tsx
@@ -1,12 +1,34 @@
+import Link from 'next/link'
+import { Layout, Row } from 'antd'
 import { Container } from '@components/layouts'
-import { Layout } from 'antd'
 
 const { Header: AntdHeader } = Layout
 
 export const Header = () => {
   return (
     <AntdHeader>
-      <Container>Header </Container>
+      <Container>
+        <Row>
+          <Link href="/">
+            <a>Shukhratbek</a>
+          </Link>
+
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>Blog</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>About</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </Row>
+      </Container>
     </AntdHeader>
   )
 }
