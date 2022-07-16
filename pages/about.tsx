9a88227157c0ca7e611331dba01759a23369220c
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-07-16 14:52:59 +0500

    feat: add about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { HeadContent } from '@components'
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <HeadContent title="About" />
+      About page
+    </div>
+  )
+}
+
+export default About
